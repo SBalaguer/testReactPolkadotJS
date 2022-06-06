43d4908525c78255f5291549a4fc942b169ea705
Santi Balaguer <santibalaguer@Santis-MacBook-Pro.local>
2022-06-06 15:40:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+const test = async function () {
+  const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  // Do something
+  console.log(api.genesisHash.toHex());
+
+}
 
 function App() {
+
+  test();
+
   return (
     <div className="App">
       <header className="App-header">
